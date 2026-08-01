b70f100ea19cc7fc620cfd9c4496959f0e1b6f36
Marshall45216 <marshall45216@yahoo.com>
2026-08-01 11:00:39 -0400

    Update CalendarScreen.kt

--- family-calendar-tv (1)/family-calendar-tv/app/src/main/java/com/familycal/tv/ui/CalendarScreen.kt
+++ family-calendar-tv (1)/family-calendar-tv/app/src/main/java/com/familycal/tv/ui/CalendarScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.tv.material3.Text
 import com.familycal.tv.data.AppState
 import com.familycal.tv.model.*
 import com.familycal.tv.ui.theme.*
